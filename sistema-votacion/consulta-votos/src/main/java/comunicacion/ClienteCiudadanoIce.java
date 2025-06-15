ff5c975a0519538bded6d3191638913f88150864
MarianaDeLaCruz06 <mariancruzpo@gmail.com>
2025-06-15 00:53:11 -0500

    con

--- sistema-votacion/consulta-votos/src/main/java/comunicacion/ClienteCiudadanoIce.java
+++ sistema-votacion/consulta-votos/src/main/java/comunicacion/ClienteCiudadanoIce.java
@@ -1,30 +1,12 @@
 package comunicacion;
 
-import VotingSystem.QueryStationPrx;
-import com.zeroc.Ice.*;
 import java.util.HashMap;
+import java.util.Map;
 
 public class ClienteCiudadanoIce {
-    private QueryStationPrx proxy;
-    private final HashMap<String, String> cache = new HashMap<>();
-
-    public ClienteCiudadanoIce(Communicator communicator) {
-        ObjectPrx base = communicator.stringToProxy("QueryStation:tcp -h localhost -p 12000");
-        proxy = QueryStationPrx.checkedCast(base);
-        if (proxy == null) {
-            throw new RuntimeException("Proxy QueryStation no válido.");
-        }
-    }
+    private final Map<String, String> cache = new HashMap<>();
 
     public String consultarLugar(String cedula) {
-        if (cache.containsKey(cedula)) {
-            return cache.get(cedula); 
-        }
-
-        String respuesta = proxy.query(cedula); 
-        if (respuesta != null) {
-            cache.put(cedula, respuesta); 
-        }
-        return respuesta;
+        return cache.computeIfAbsent(cedula, c -> ServiceLocator.getQueryStation().query(c));
     }
 }
